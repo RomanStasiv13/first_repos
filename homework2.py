2c3c0095432fdbe389f6e1f52da7692e9c2a377f
neferpitou <neverdiesnightmare131313@gmail.com>
2021-11-22 20:56:35 +0200

    Homework2

---
+++ homework2.py
@@ -0,0 +1,25 @@
+###Task 1
+###String manipulation
+###Write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
+###If the string length is less than 2, return instead of the empty string.
+###Sample String: 'helloworld'
+###Expected Result : 'held'
+###Sample String: 'my'
+###Expected Result : 'mymy'
+###Sample String: ' x'
+###Expected Result: Empty String
+###Tips:
+###Use built-in function len() on an input string
+###Use positive indexing to get the first characters of a string and negative indexing to get the last characters
+
+
+###Task 2
+###The valid phone number program.
+###Make a program that checks if a string is in the right format for a phone number.
+###The program should check that the string contains only numerical characters and is only 10 characters long.
+###Print a suitable message depending on the outcome of the string evaluation.
+
+###Task 3
+###The name check.
+###The program should check if your input is equal to the stored name even
+###if the given name has another case, e.g., if your input is “Anton” and the stored name is “anton”, it should return True.
